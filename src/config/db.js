e5d97a2adb7d6e63062ddb3fad6a30239b7422ed
Felipe <oliveira.felipe28@gmail.com>
2022-07-13 12:34:41 -0300

    LOG: Criacão do arquivo de registro de LOGS

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,8 @@
 import Sequelize from 'sequelize';
+import dotenv from 'dotenv'
+dotenv.config()
+
+
 
   //*// CRIANDO A CONEXÃO COM O BANCO DE DADOS //*//
 
@@ -6,6 +10,9 @@ const database = new Sequelize(process.env.DB_DATABASE_NAME, process.env.DB_USER
     host: process.env.DB_HOST,  
     dialect: "mysql"
   });
+
+console.log(process.env.DB_DATABASE_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, process.env.DB_HOST)
+
 
 export default database
 
